ead718333b85b59e0177251aeedc590ac9565591
MAbella <miriam.abella@lifullconnect.com>
2020-04-23 19:59:49 +0200

    Refactor: inlined coordinates privat setter

--- MarsRover/NewMarsRovers/src/main/java/MarsRover.java
+++ MarsRover/NewMarsRovers/src/main/java/MarsRover.java
@@ -9,48 +9,44 @@ public class MarsRover {
         this.coordinates = new Coordinates(x, y);
     }
 
-    private void setCoordinates(int x, int y){
-           this.coordinates = new Coordinates(x, y);
-    }
-
     public void receive(String commandsSequence) {
         if (commandsSequence.equals("")) {
             return;
         }
         if (direction.equals("N")) {
-            setCoordinates(coordinates.x(),coordinates.y() + 1);
+            this.coordinates = new Coordinates(coordinates.x(), coordinates.y() + 1);
         }
         else if (direction.equals("E")) {
-            setCoordinates(coordinates.x() + 1, coordinates.y());
+            this.coordinates = new Coordinates(coordinates.x() + 1, coordinates.y());
         }else if (direction.equals("S")){
-            setCoordinates(coordinates.x(), coordinates.y() - 1);;
+            this.coordinates = new Coordinates(coordinates.x(), coordinates.y() - 1);
+            ;
         }else {
-            setCoordinates(coordinates.x() - 1, coordinates.y());
+            this.coordinates = new Coordinates(coordinates.x() - 1, coordinates.y());
         }
 
     }
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         MarsRover marsRover = (MarsRover) o;
-        return coordinates.x() == marsRover.coordinates.x() &&
-                coordinates.y() == marsRover.coordinates.y() &&
-                Objects.equals(direction, marsRover.direction);
+        return Objects.equals(direction, marsRover.direction) &&
+                Objects.equals(coordinates, marsRover.coordinates);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(coordinates.x(), coordinates.y(), direction);
+        return Objects.hash(direction, coordinates);
     }
 
     @Override
     public String toString() {
         return "MarsRover{" +
-                "x=" + coordinates.x() +
-                ", y=" + coordinates.y() +
-                ", direction=" + direction +
+                "direction='" + direction + '\'' +
+                ", coordinates=" + coordinates +
                 '}';
     }
 }
