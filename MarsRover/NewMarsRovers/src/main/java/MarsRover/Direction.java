fb75b6d2478726f309d4d00c9a2d8041cece64e3
MAbella <miriam.abella@lifullconnect.com>
2020-04-25 13:10:29 +0200

    creating enum for directtions. Feature: Direction can point North

---
+++ MarsRover/NewMarsRovers/src/main/java/MarsRover/Direction.java
@@ -0,0 +1,9 @@
+package MarsRover;
+
+public enum Direction {
+    NORTH;
+
+    public static Direction pointingTo(String direction) {
+        return NORTH;
+    }
+}
---
+++ MarsRover/NewMarsRovers/src/test/java/MarsRoversTests/DirectionsCreationTest.java
@@ -0,0 +1,15 @@
+package MarsRoversTests;
+
+import MarsRover.Direction;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DirectionsCreationTest {
+    @Test
+    public void direction_pointing_north(){
+        Direction direction = Direction.pointingTo("N");
+
+        assertEquals(direction, Direction.NORTH);
+    }
+}
