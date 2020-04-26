ebfd7cf4f97d1a8cf4f5fb6294994b5612635199
MAbella <miriam.abella@lifullconnect.com>
2020-04-26 12:42:06 +0200

    Refactor: simplifyion movement commands creation

--- MarsRover/NewMarsRovers/src/main/java/MarsRover/commands/CommandCodeInterpreter.java
+++ MarsRover/NewMarsRovers/src/main/java/MarsRover/commands/CommandCodeInterpreter.java
@@ -3,22 +3,20 @@ package MarsRover.commands;
 import MarsRover.Command;
 
 public class CommandCodeInterpreter {
+    private static final int DISPLACEMENT_LENGHT = 1;
     public static Command interprete(String commandCode){
         if (isRightRotation(commandCode)) {
             return new RightRotation();
         } else if (isLeftRotation(commandCode)) {
             return new LeftRotation();
-        } else if(isMovement(commandCode)) {
-            int displacement = getDisplacement(commandCode);
-            return new Movement(displacement);
+        } else if(isForwardMovement(commandCode) ) {
+            return new Movement(DISPLACEMENT_LENGHT);
+        } else if( isBackwardMovement(commandCode)) {
+            return new Movement(-DISPLACEMENT_LENGHT);
         }else {
             return new UnknownCommand();
         }
 
-    }
-
-    private static boolean isMovement(String commandCode) {
-        return isForwardMovement(commandCode)  ||  isBackwardMovement(commandCode);
     }
 
     private static boolean isForwardMovement(String commandCode) {
@@ -37,12 +35,4 @@ public class CommandCodeInterpreter {
         return commandCode.equals("r");
     }
 
-    private static int getDisplacement(String commandCode) {
-        final int DISPLACEMENT_LENGHT = 1;
-        int displacement = -DISPLACEMENT_LENGHT;
-        if (commandCode.equals("f")) {
-            displacement = DISPLACEMENT_LENGHT;
-        }
-        return displacement;
-    }
 }
